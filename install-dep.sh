380e6d7bb40dc474693f195d05218c7f0c1bd758
Felix Vadcard <vadcard.felix@gmail.com>
2026-01-06 21:15:58 +0100

    some fixes

--- install-dep.sh
+++ install-dep.sh
@@ -5,19 +5,21 @@ sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
+cd ..
 
 # ghostty
-sudo pacman -S zsh ghostty neovim waybar starship bat fzf zoxide gitui 
+sudo pacman -S zsh ghostty neovim waybar starship bat fzf zoxide gitui eza 
 
 # rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sudo pacman -S zsh-autosuggestions zsh-syntax-highlighting
 
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-nvm use latest
+# nvm use latest
 
 # sjvm
 # TODO i need to make a default configuration that i store also in dot files 
