7f48625f21f4ffc1316548be506702e6d21eeb48
Felix Vadcard <vadcard.felix@gmail.com>
2026-02-14 09:29:32 +0100

    fix removed agent from scripts

--- .config/opencode/exemple_agents/exemple_setup/planner/install.sh
+++ .config/opencode/exemple_agents/exemple_setup/planner/install.sh
@@ -1,7 +1,6 @@
 rm ~/.config/opencode/agents/Planner.md
 rm ~/.config/opencode/agents/feature-designer.md
 rm ~/.config/opencode/agents/feature-reviewer.md
-rm ~/.config/opencode/agents/feature-writer.md
 rm ~/.config/opencode/agents/context-gatherer.md
 ln -s $(pwd)/Planner.md ~/.config/opencode/agents/Planner.md
 ln -s $(pwd)/subagents/* ~/.config/opencode/agents/
--- .config/opencode/exemple_agents/exemple_setup/planner/uninstall.sh
+++ .config/opencode/exemple_agents/exemple_setup/planner/uninstall.sh
@@ -1,5 +1,4 @@
 rm ~/.config/opencode/agents/Planner.md
 rm ~/.config/opencode/agents/feature-designer.md
 rm ~/.config/opencode/agents/feature-reviewer.md
-rm ~/.config/opencode/agents/feature-writer.md
 rm ~/.config/opencode/agents/context-gatherer.md
